a1d1a1f8cd90f5d55209546643e947f05d235063
Arsenii K <167592505+Arseny89@users.noreply.github.com>
2024-07-05 19:48:53 -0700

    Create Rovers.swift

---
+++ NASARover/Models/Rovers.swift
@@ -0,0 +1,14 @@
+//
+//  Rovers.swift
+//  NASARover
+//
+//  Created by Арсений Корниенко on 7/5/24.
+//
+
+import Foundation
+
+enum Rovers: String {
+    case curiosity
+    case opportunity
+    case spirit
+}
