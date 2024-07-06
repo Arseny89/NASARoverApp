42fbfeee1369801576fc17e8d07a8e433ea804be
Arsenii K <167592505+Arseny89@users.noreply.github.com>
2024-07-05 18:26:43 -0700

    Initial Commit

---
+++ NASARover/NASARoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  NASARoverApp.swift
+//  NASARover
+//
+//  Created by Арсений Корниенко on 7/5/24.
+//
+
+import SwiftUI
+
+@main
+struct NASARoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
