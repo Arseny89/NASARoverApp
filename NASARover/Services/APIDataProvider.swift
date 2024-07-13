ceabbc543fb8f1f1b1acb8c60d0f9c5e2f3b74e6
Arsenii K <167592505+Arseny89@users.noreply.github.com>
2024-07-12 23:07:56 -0700

    Update APIDataProvider.swift

--- NASARover/Services/APIDataProvider.swift
+++ NASARover/Services/APIDataProvider.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import Alamofire
+import Combine
 
 enum AppError: Error, Decodable {
     case connectivityError
@@ -69,5 +70,37 @@ final class APIDataProvider {
                     errorHandler(.other(error.localizedDescription))
                 }
             }
+    }
+    
+    func request<T: Decodable>(for endpoint: Endpoint) -> AnyPublisher<T, AppError> {
+        return URLSession.shared
+            .dataTaskPublisher(for: endpointProvider.getURL(for: endpoint))
+            .subscribe(on: DispatchQueue.global(qos: .background))
+            .receive(on: DispatchQueue.main)
+            .mapError{ _ in .connectivityError }
+            .flatMap { data, response -> AnyPublisher<T, AppError> in
+                let decoder = JSONDecoder()
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
+                if let response = response as? HTTPURLResponse {
+                    if (200...299) ~= response.statusCode {
+                        return Just(data)
+                            .decode(type: T.self, decoder: decoder)
+                            .mapError { error in .decodeJSONfailed }
+                            .eraseToAnyPublisher()
+                    } else {
+                        do {
+                            let errorResponse = try decoder.decode(APIErrorResponse.self, from: data)
+                            return Fail(error: .apiError(errorResponse.error))
+                                .eraseToAnyPublisher()
+                        } catch {
+                            return Fail(error: .decodeJSONfailed)
+                                .eraseToAnyPublisher()
+                        }
+                    }
+                }
+                return Fail(error: .unknown)
+                    .eraseToAnyPublisher()
+            }
+            .eraseToAnyPublisher()
     }
 }
