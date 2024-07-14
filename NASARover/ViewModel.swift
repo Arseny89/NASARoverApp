23d62c2d12e48a72d8818302db6343dd581cc193
Arsenii K <167592505+Arseny89@users.noreply.github.com>
2024-07-13 23:03:48 -0700

    Update ViewModel.swift

--- NASARover/ViewModel.swift
+++ NASARover/ViewModel.swift
@@ -9,24 +9,20 @@ import Foundation
 import Combine
 
 final class ViewModel: ObservableObject {
+    @Published var photoDict: [Int: URL] = [:]
     private let photoProvider: PhotosProvider
     private let rover: Rovers
-    
-    @Published var photoDict: [Int: URL] = [:]
     private var cancellables = Set<AnyCancellable>()
     
     init(for rover: Rovers) {
         self.rover = rover
         self.photoProvider = PhotosProviderImpl(for: rover)
-        
-        bind()
     }
     
     func bind() {
         photoProvider.fetchPhotos()
             .sink { completion in
-                
-                guard case .failure(let error) = completion else { return }
+                guard case .failure(_) = completion else { return }
             } receiveValue: { [weak self] photos in
                 guard let self else { return }
                 photoDict = photos
