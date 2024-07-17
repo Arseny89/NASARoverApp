5339677d4592b32354ccfab6bb8dc2a4ab4e5d2c
Arsenii K <167592505+Arseny89@users.noreply.github.com>
2024-07-16 22:33:55 -0700

    Create Titles.swift

---
+++ NASARover/Models/Titles.swift
@@ -0,0 +1,15 @@
+//
+//  Titles.swift
+//  NASARover
+//
+//  Created by Арсений Корниенко on 7/14/24.
+//
+
+import Foundation
+
+enum Titles: String {
+    case roverSelectionView = "fetch foto from rover"
+    case roverInfoSubtitle = "mission"
+    case moreButton = "more"
+    case fetchButton = "fetch all"
+}
