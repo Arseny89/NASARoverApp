6c1acc437e4f14d4d7ec999fc976c5bf90719eff
Arsenii K <167592505+Arseny89@users.noreply.github.com>
2025-01-26 19:05:31 -0800

    Update NASARoverApp.swift

--- NASARover/NASARoverApp.swift
+++ NASARover/NASARoverApp.swift
@@ -6,12 +6,24 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct NASARoverApp: App {
+    
+    let modelContainer: ModelContainer
+        
+        init() {
+            do {
+                modelContainer = try ModelContainer(for: Photo.self)
+            } catch {
+                fatalError("Could not initialize ModelContainer")
+            }
+        }
     var body: some Scene {
         WindowGroup {
             RoverSelectionView(viewModel: RoverSelectionViewModel(for: .curiosity))
+                .modelContainer(modelContainer)
         }
     }
 }
