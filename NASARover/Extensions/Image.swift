ed5de2d6456a848f6989898229aedb846c347082
Arsenii K <167592505+Arseny89@users.noreply.github.com>
2025-01-26 19:05:18 -0800

    Create Image.swift

---
+++ NASARover/Extensions/Image.swift
@@ -0,0 +1,33 @@
+//
+//  Image.swift
+//  NASARover
+//
+//  Created by Арсений Корниенко on 7/16/24.
+//
+
+import Foundation
+import SwiftUI
+
+extension Image {
+    
+    init(icon: Icons) {
+        self.init(systemName: icon.rawValue)
+    }
+    
+    init(image: Images) {
+        self.init(image.rawValue)
+    }
+    
+    enum Icons: String {
+        case ellipsisCircle = "ellipsis.circle"
+        case calendar = "calendar"
+        case xmark = "xmark"
+        case add = "plus"
+    }
+    
+    enum Images: String {
+        case curiosity = "curiosity_shape_bg"
+        case opportunity = "opportunity_shape_bg"
+        case spirit = "spirit_shape_bg"
+    }
+}
