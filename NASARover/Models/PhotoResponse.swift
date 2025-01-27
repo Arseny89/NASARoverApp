4f501defac40e79d21ae869642855ea4f93fb384
Arsenii K <167592505+Arseny89@users.noreply.github.com>
2025-01-26 19:05:28 -0800

    Update PhotoResponse.swift

--- NASARover/Models/PhotoResponse.swift
+++ NASARover/Models/PhotoResponse.swift
@@ -11,17 +11,25 @@ struct PhotoResponse: Decodable {
     let photos: [PhotoData]
 }
 
+struct RoverInfo: Decodable {
+    let name: String
+}
+
 struct PhotoData: Decodable {
+    let rover: RoverInfo
     let id: Int
     let sol: Int
     let imageURL: URL
     let date: String
+    let camera: Camera
     
     enum CodingKeys: String, CodingKey {
+        case rover
         case id
         case sol
         case imageSource = "imgSrc"
         case date = "earthDate"
+        case camera
     }
     
     init(from decoder: any Decoder) throws {
@@ -31,5 +39,11 @@ struct PhotoData: Decodable {
         let imageSource = try container.decode(URL.self, forKey: .imageSource)
         self.imageURL = URL(string: "\(imageSource)") ?? URL.homeDirectory
         self.date = try container.decode(String.self, forKey: .date)
+        self.camera = try container.decode(Camera.self, forKey: .camera)
+        self.rover = try container.decode(RoverInfo.self, forKey: .rover)
     }
+}
+
+struct Camera: Decodable {
+    let fullName: String
 }
